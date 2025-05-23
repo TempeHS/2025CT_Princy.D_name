482554634327d9f8c55d9f5f490c89fdce3f49b9
Princy-Dumais <princy.dumais@education.nsw.gov.au>
2025-05-23 15:03:46 +1000

    created start of dashing mechanic, made a second test level

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 using TMPro;
 // CLOSE UNITY BEFORE COMMITING.
 public class PlayerController : MonoBehaviour
-{   
- // The Float variable is used for a variable. 
+{
+    // The Float variable is used for a variable. 
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 10f;
+    private float jumpingPower = 18f;
     private bool isFacingRight = true;
     private int count;
     public TextMeshProUGUI countText;
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
 
-// The SerializeField references the RigidBody, the Groundcheck and the Groundlayer.
+    // The SerializeField references the RigidBody, the Groundcheck and the Groundlayer.
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
 
     // Update is called once per frame
     void Start()
@@ -28,7 +34,7 @@ public class PlayerController : MonoBehaviour
     {
         // Returns a value of 0, -1 or 1 depending on where the player is facing. 
         horizontal = Input.GetAxisRaw("Horizontal");
-        
+
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
@@ -76,5 +82,20 @@ public class PlayerController : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Velocity2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
