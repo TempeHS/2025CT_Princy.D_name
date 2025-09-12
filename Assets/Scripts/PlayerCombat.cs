6a2b1967e59f66df6939d2d9920dd2a399376050
Princy D <princy.dumais@education.nsw.gov.au>
2025-09-12 15:02:51 +1000

    how do i size down the sprite

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
     {
         
     }
