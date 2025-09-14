b6578dc1f81d215a005d3f38a81c28c33283f694
Princy-Dumais <princy.dumais@education.nsw.gov.au>
2025-09-15 09:38:22 +1000

    Creating an attack script

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -19,7 +19,6 @@ public class EnemyProjectile : MonoBehaviour
     {
 
         float distance = Vector2.Distance(transform.position, player.transform.position);
-        Debug.Log(distance);
 
         if (distance < 4)
         {
--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour
 {
 
-    // Update is called once per frame
+    public Animator animator;
+    public Transform AP;
+    // AP is Attack Point 
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers; // LayerMasks are applied in order to define who the enemies are.
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
@@ -16,6 +21,13 @@ public class PlayerCombat : MonoBehaviour
 
     void Attack()
     {
-        
+        Debug.Log("City");
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AP.position, attackRange, enemyLayers); // Creates a circle from the AP and the attack range value, and returns what the circle interacted with.
+        // -- DAMAGE -- 
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
     }
 }
