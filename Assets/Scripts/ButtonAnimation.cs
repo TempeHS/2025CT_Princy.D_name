e5adecf44e2a170aa5f1c61f06a0ff64ad172e4a
Princy D <princy.dumais@education.nsw.gov.au>
2025-09-25 15:03:45 +1000

    animation button script somehow broke??

---
+++ Assets/Scripts/ButtonAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    Animator anim;
+    public GameObject Button;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Active");
+            Button.SetActive(true);
+        }
+    }
+}
